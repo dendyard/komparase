43ace1b124ccdc617b25649b1a57f571cfb1c75a
dendyard <dendyardany@gmail.com>
2020-10-01 00:31:57 +0700

    Rollback URI

--- system/core/URI.php
+++ system/core/URI.php
@@ -168,11 +168,10 @@ class CI_URI {
 
 			$this->segments[0] = NULL;
 			// Populate the segments array
-			foreach (explode('/', trim(str_replace("'","", $this->uri_string), '/')) as $val)
+			foreach (explode('/', trim($this->uri_string, '/')) as $val)
 			{
 				$val = trim($val);
 				// Filter segments for security
-                
 				$this->filter_uri($val);
 
 				if ($val !== '')
@@ -326,12 +325,9 @@ class CI_URI {
 	 */
 	public function filter_uri(&$str)
 	{
-        
-        if ( ! empty($str) && ! empty($this->_permitted_uri_chars) && ! preg_match('/^['.$this->_permitted_uri_chars.']+$/i'.(UTF8_ENABLED ? 'u' : ''), $str))
+		if ( ! empty($str) && ! empty($this->_permitted_uri_chars) && ! preg_match('/^['.$this->_permitted_uri_chars.']+$/i'.(UTF8_ENABLED ? 'u' : ''), $str))
 		{
 			show_error('The URI you submitted has disallowed characters.', 400);
-            //return implode('/', $str);
-            
 		}
 	}
 
